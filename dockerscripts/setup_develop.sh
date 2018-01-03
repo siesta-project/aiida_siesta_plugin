c51bac7ea6b24c23727b1819b46c227bde42d0cd
Vladimir <dikan.va@ya.ru>
2018-01-04 00:37:27 +0100

    added comments for containers setup script

--- dockerscripts/setup_develop.sh
+++ dockerscripts/setup_develop.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-export C_FORCE_ROOT=1
+export C_FORCE_ROOT=1  # environment variable to be able to run
+                       # aiida daemon under root inside the container
 
+# Check if the container is set up.
+# That means that it is re-used by docker-compose.
 if [ -e ~/.aiida/config.json ]
 then
     echo "Aiida development environment is set up."
 else
+    # ..if not, let's configure it!
+    # First, install siesta plugin from the mounted volume:
     pip install -e /code/siesta_plugin/
 
+    # Setup default aiida profile:
     verdi setup --non-interactive \
           --backend=django \
           --email="aiida@localhost" \
@@ -20,21 +26,31 @@ else
           --institution="None" \
           --no-password
 
+    # Set default profile for `verdi` command and the daemon;
+    # Then start the daemon:
     verdi profile setdefault verdi default
     verdi profile setdefault daemon default
     verdi daemon start
 
+    # Setup the container's localhost as the test computer for aiida.
+    # It's label will be called `develop`:
     cat /code/siesta_plugin/dockerscripts/computer-setup.txt | verdi computer setup
     verdi computer configure develop
     verdi computer test develop
 
+    # Setup the container's SIESTA code executable as the test code for aiida.
+    # It will be labeled `siesta@develop`:
     cat /code/siesta_plugin/dockerscripts/code-setup.txt | verdi code setup
 
+    # Enable verdi auto-completion inside the container:
     echo "eval \"\$(verdi completioncommand)"\" >> ~/.bashrc
 fi
 
-find / -name \*.pyc -delete
-reentry scan -r aiida
+find / -name \*.pyc -delete  # remove python bytecode from everywhere
+                             # (some of it could point to ghost places)
 
-verdi daemon restart
-verdi daemon logshow
+reentry scan -r aiida  # to make sure that `reentry` finds everything
+
+verdi daemon restart   # restart the daemon
+verdi daemon logshow   # eventually, launch the daemon continuous log show
+                       # as the `service` to prevent shutdown of the dev container
