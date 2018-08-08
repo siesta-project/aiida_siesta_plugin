414548874e9a9ca8730c4083a7af50411a3ae02a
bosonie <bosonie@tcd.ie>
2018-08-08 17:20:36 +0100

    almost done

--- aiida_siesta/examples/plugins/siesta/test_siesta_si_bands.py
+++ aiida_siesta/examples/plugins/siesta/test_siesta_si_bands.py
@@ -8,9 +8,14 @@ __contributors__ = "Andrea Cepellotti, Victor Garcia-Suarez, Alberto Garcia, Ema
 
 import sys
 import os
-
+from aiida.tools import get_explicit_kpoints_path
 from aiida.common.example_helpers import test_and_get_code
 from aiida.common.exceptions import NotExistent
+
+#In this example we will calculate the band structure of Si.
+#Thanks to SeeK-path we can automatically generate the 
+#high symmetry points path where to calculate the bands.
+#Alternatively, a manual path or list of k-points can be set.
 
 ################################################################
 
@@ -35,16 +40,46 @@ except IndexError:
 try:
     codename = sys.argv[2]
 except IndexError:
-    codename = 'Siesta-4.0@rinaldo'
+    codename = 'siesta4.0.1@parsons'
 
-# If True, load the pseudos from the family specified below
-# Otherwise, use static files provided
-auto_pseudos = True
+#
+##----------Set calculation----------------------
+#For remote codes, it is not necessary to manually set the computer,
+#since it is set automatically by new_calc
+#computer = code.get_remote_computer()
+#calc = code.new_calc(computer=computer)
+code = test_and_get_code(codename, expected_code_type='siesta.siesta')
+calc = code.new_calc()
+calc.label = "Si_bulk"
+calc.description = "Siesta test calculation. Si bulk + automatic bands"
+calc.set_max_wallclock_seconds(30*60) # 30 min
+#//////////// clarify this /////////////////////
+#Valid only for Slurm and PBS (using default values for the number_cpus_per_machine), change for SGE-like schedulers 
+#Otherwise, to specify a given # of cpus per machine, uncomment the following:
+#calc.set_resources({"num_machines": 1, "num_mpiprocs_per_machine": 8})
+#calc.set_resources({"parallel_env": 'openmpi',"tot_num_mpiprocs": 1,"num_machines": 1,"num_cpus": 2})
+#/////////////clarify this////////////////
+calc.set_resources({"num_machines": 1})
+#calc.set_custom_scheduler_commands("#SBATCH --account=ch3")
 
 queue = None
-settings = None
+if queue is not None:
+    calc.set_queue_name(queue)
 
-code = test_and_get_code(codename, expected_code_type='siesta.siesta')
+#
+##--------------Settings------------------
+#The object settings is optional.
+#settings_dict={'test_key': 'test_value'}
+#settings = ParameterData(dict=settings_dict)
+settings = None
+if settings is not None:
+    calc.use_settings(settings)
+
+#
+##-------------------Structure-----------------------------------
+#Manually set the structure, all the quantities must be in Ang.
+#Then, we pass through SeeK-path, to get the standardized cell,
+#necessary for the automatic choice of the bands path.
 
 alat = 5.430 # angstrom
 cell = [[0.5*alat, 0.5*alat, 0.,],
@@ -52,16 +87,21 @@ cell = [[0.5*alat, 0.5*alat, 0.,],
         [0.5*alat, 0., 0.5*alat,],
        ]
 
-# Si
-# This was originally given in the "ScaledCartesian" format
-#
 s = StructureData(cell=cell)
 s.append_atom(position=(0.000*alat,0.000*alat,0.000*alat),symbols=['Si'])
 s.append_atom(position=(0.250*alat,0.250*alat,0.250*alat),symbols=['Si'])
-
-
 elements = list(s.get_symbols_set())
+seekpath_parameters = ParameterData(dict={'reference_distance': 0.02,'symprec': 0.0001})
+result=get_explicit_kpoints_path(s, **seekpath_parameters.get_dict())
+newstructure = result['primitive_structure']
+calc.use_structure(newstructure)
 
+
+#
+##---------------------Pseudos---------------------------
+#If auto_pseudos = True, load the pseudos from the family specified
+#below. Otherwise, use static files provided
+auto_pseudos = False
 if auto_pseudos:
     valid_pseudo_groups = PsfData.get_psf_groups(filter_elements=elements)
 
@@ -84,6 +124,32 @@ if auto_pseudos:
         print >> sys.stderr, ",".join(i.name for i in valid_pseudo_groups)
         sys.exit(1)
 
+if auto_pseudos:
+    try:
+        calc.use_pseudos_from_family(pseudo_family)
+        print "Pseudos successfully loaded from family {}".format(pseudo_family)
+    except NotExistent:
+        print ("Pseudo or pseudo family not found. You may want to load the "
+               "pseudo family, or set auto_pseudos to False.")
+        raise
+else:
+    raw_pseudos = [("Si.psf", 'Si')]
+
+    for fname, kinds, in raw_pseudos:
+      absname = os.path.realpath(os.path.join(os.path.dirname(__file__),
+                                            "data",fname))
+      pseudo, created = PsfData.get_or_create(absname,use_first=True)
+      if created:
+        print "Created the pseudo for {}".format(kinds)
+      else:
+        print "Using the pseudo for {} from DB: {}".format(kinds,pseudo.pk)
+
+      # Attach pseudo node to the calculation
+      calc.use_pseudo(pseudo,kind=kinds)
+
+
+#
+##----------Calculation parameters----------------
 parameters = ParameterData(dict={
                 'xc:functional': 'LDA',
                 'xc:authors': 'CA',
@@ -104,103 +170,61 @@ parameters = ParameterData(dict={
                 'xml:write': True,
                 'dm:usesavedm': True
                 })
+calc.use_parameters(parameters)
 
+
+#
+##-----------------Basis-set-------------------
 basis = ParameterData(dict={
 'pao-energy-shift': '300 meV',
 '%block pao-basis-sizes': """
 Si DZP                    """,
 })
+calc.use_basis(basis)
 
+
+#
+##------------KPoints for calculation---------
 kpoints = KpointsData()
-
 # method mesh
 kpoints_mesh = 4
 kpoints.set_kpoints_mesh([kpoints_mesh,kpoints_mesh,kpoints_mesh])
-
-
-# (the object settings is optional)
-settings_dict={'test_key': 'test_value'}
-settings = ParameterData(dict=settings_dict)
-
-## For remote codes, it is not necessary to manually set the computer,
-## since it is set automatically by new_calc
-#computer = code.get_remote_computer()
-#calc = code.new_calc(computer=computer)
-
-calc = code.new_calc()
-calc.label = "Si_bulk"
-calc.description = "Siesta test calculation. Si bulk + automatic bands"
-calc.set_max_wallclock_seconds(30*60) # 30 min
-
-#------------ clarify this
-# Valid only for Slurm and PBS (using default values for the
-# number_cpus_per_machine), change for SGE-like schedulers 
-## Otherwise, to specify a given # of cpus per machine, uncomment the following:
-# calc.set_resources({"num_machines": 1, "num_mpiprocs_per_machine": 8})
-#calc.set_resources({"parallel_env": 'openmpi',"tot_num_mpiprocs": 1,"num_machines": 1,"num_cpus": 2})
-#------------ clarify this
-calc.set_resources({"num_machines": 1})
-
-#calc.set_custom_scheduler_commands("#SBATCH --account=ch3")
-
-if queue is not None:
-    calc.set_queue_name(queue)
-
-calc.use_structure(s)
-calc.use_parameters(parameters)
-calc.use_basis(basis)
-
-if auto_pseudos:
-    try:
-        calc.use_pseudos_from_family(pseudo_family)
-        print "Pseudos successfully loaded from family {}".format(pseudo_family)
-    except NotExistent:
-        print ("Pseudo or pseudo family not found. You may want to load the "
-               "pseudo family, or set auto_pseudos to False.")
-        raise
-else:
-    raw_pseudos = [("Si.psf", 'Si')]
-
-    for fname, kinds, in raw_pseudos:
-      absname = os.path.realpath(os.path.join(os.path.dirname(__file__),
-                                            "data",fname))
-      pseudo, created = PsfData.get_or_create(absname,use_first=True)
-      if created:
-        print "Created the pseudo for {}".format(kinds)
-      else:
-        print "Using the pseudo for {} from DB: {}".format(kinds,pseudo.pk)
-        
-      # Attach pseudo node to the calculation
-      calc.use_pseudo(pseudo,kind=kinds)
-
 calc.use_kpoints(kpoints)
 
-# K-points for bands, uncomment your favourite  --------------------
-# NOTE: bandskpoints.set_cell(s.cell, s.pbc) HAS TO BE SET ALWAYS ###
+
+#-------------------K-points for bands --------------------
 bandskpoints = KpointsData()
+#Uncomment your favourite, three options:
 
-##..Set a path, label needed, 40 is number of kp between W-L and between L-G..##
-#kpp = [('W',  (0.500,  0.250, 0.750), 'L', (0.500,  0.500, 0.500), 40),
-#        ('L', (0.500,  0.500, 0.500), 'G', (0., 0., 0.), 40)]
-#bandskpoints.set_cell(s.cell, s.pbc)
-#bandskpoints.set_kpoints(kpp)
+#1)
+##.....Making use of SeeK-path for the automatic path......
+##The choice of the distance between kpoints is in the call seekpath_parameters
+##All high symmetry points included, labels already included
+#bandskpoints=result['explicit_kpoints']
 
-
-##..........................Only points, no labels............................##
+#2)
+##.....Only points, no labels.......
 #kpp = [(0.500,  0.250, 0.750), (0.500,  0.500, 0.500), (0., 0., 0.)]
-#bandskpoints.set_cell(s.cell, s.pbc)
 #bandskpoints.set_kpoints(kpp)
 
-##..kp path automatically generated from structure (all high-simmetry point)..##
-##.....labels automatically included, 0.05 is the distance between kpoints....##
-# Not available in 0.5.0...
-bandskpoints.set_cell(s.cell, s.pbc)
-bandskpoints.set_kpoints_path(kpoint_distance = 0.05)
+#3)
+##.....Set a manual path.......
+##Labels needed, 40 is number of kp between W-L and between L-G...
+##With new version of aiida, the use of this functionality is a bit involved.
+##First we have to call a function in tools.data.array.kpoints.legacy,
+##The funcion is called get_explicit_kpoints_path, but is not the same
+##function I used before.
+##Second step is to assign the results of the funtion to a KpointsData()
+kpp = [('W',  (0.500,  0.250, 0.750), 'L', (0.500,  0.500, 0.500), 40),
+        ('L', (0.500,  0.500, 0.500), 'G', (0., 0., 0.), 40)]
+from aiida.tools.data.array.kpoints import legacy
+fs=legacy.get_explicit_kpoints_path(kpp)
+bandskpoints=KpointsData()
+bandskpoints.set_kpoints(fs[3])
+bandskpoints.labels=fs[4]
 
 calc.use_bandskpoints(bandskpoints)
 
-if settings is not None:
-    calc.use_settings(settings)
 #from aiida.orm.data.remote import RemoteData
 #calc.set_outdir(remotedata)
 
