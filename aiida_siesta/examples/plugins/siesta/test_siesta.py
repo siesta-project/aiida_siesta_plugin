ce7f8c2625a19297454e601fb0523fef580ebdd5
Vladimir <vladimir.dikan@icn2.cat>
2019-01-24 12:01:24 +0100

    corrected keywords in examples

--- aiida_siesta/examples/plugins/siesta/test_siesta.py
+++ aiida_siesta/examples/plugins/siesta/test_siesta.py
@@ -134,7 +134,7 @@ calc.use_structure(s)
 params_dict= {
 'xc.functional': 'LDA',
 'xc.authors': 'CA',
-'spin:polarized': True,
+'spin-polarized': True,
 'noncollinearspin': False,
 'mesh-cutoff': '200.000 Ry',
 'max-scfiterations': 1000,
@@ -203,7 +203,7 @@ for fname, kinds, in raw_pseudos:
         print "Created the pseudo for {}".format(kinds)
     else:
         print "Using the pseudo for {} from DB: {}".format(kinds,pseudo.pk)
-        
+
     # Attach pseudo node to the calculation
     calc.use_pseudo(pseudo,kind=kinds)
 #-------------------------------------------------------------------
@@ -229,4 +229,3 @@ else:
     calc.submit()
     print "submitted calculation; calc=Calculation(uuid='{}') # ID={}".format(
         calc.uuid,calc.dbnode.pk)
-
