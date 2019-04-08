06fb96e2f5e4414e7488dc61528298535a643c68
Vladimir <vladimir.dikan@icn2.cat>
2019-04-08 23:25:45 +0200

    main modules versions bumped

--- aiida_siesta/calculations/siesta.py
+++ aiida_siesta/calculations/siesta.py
@@ -20,7 +20,7 @@ import six
 
 __copyright__ = u"Copyright (c), 2015, ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE (Theory and Simulation of Materials (THEOS) and National Centre for Computational Design and Discovery of Novel Materials (NCCR MARVEL)), Switzerland and ROBERT BOSCH LLC, USA. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file"
-__version__ = "0.9.10"
+__version__ = "1.0.0"
 __contributors__ = "Victor M. Garcia-Suarez, Alberto Garcia, Emanuele Bosoni"
 
 
