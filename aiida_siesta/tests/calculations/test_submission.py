3974727f449eb9f5c30ff15d8564203106a58dd2
Bosoni <bosonie@tcd.ie>
2019-05-01 23:40:13 +0100

    test_submission added label

--- aiida_siesta/tests/calculations/test_submission.py
+++ aiida_siesta/tests/calculations/test_submission.py
@@ -30,7 +30,7 @@ structure.append_atom(position=(0.000 * alat, 0.000 * alat, 0.000 * alat),
 structure.append_atom(position=(0.250 * alat, 0.250 * alat, 0.250 * alat),
                       symbols=['Si'])
 
-code = load_code("siestampi@new")
+code = load_code("Siesta4.0.1@kelvin")
 
 # def test_simple_submission(siesta_develop):
     # """test that single calculation is submitted."""
@@ -58,6 +58,7 @@ parameters = Dict(dict={
     'md-numcgsteps': 3,
     'md-maxcgdispl': '0.1 Ang',
     'md-maxforcetol': '0.04 eV/Ang',
+    'write-forces': True,
     # 'xml-write': True
 })
 
@@ -83,7 +84,7 @@ for fname, kind in raw_pseudos:
 
 
 options = {
-    "max_wallclock_seconds" : 3600,
+    "max_wallclock_seconds" : 360,
     "resources" : {
         "num_machines": 1,
         "num_mpiprocs_per_machine": 1,
@@ -91,6 +92,7 @@ options = {
 }
 
 inputs = {
+    'label' : Str("Silicon bulk"),
     'structure': structure,
     'parameters': parameters,
     'code': code,
@@ -105,52 +107,3 @@ inputs = {
 }
 
 run(SiestaCalculation, **inputs)
-
-    # calc = code.new_calc(
-    #     max_wallclock_seconds=3600,
-    #     resources={
-    #         "num_machines": 1,
-    #         "num_mpiprocs_per_machine": 1,
-    #     })
-
-    # calc.label = "Si bulk"
-    # calc.description = "Test calculation with the Siesta code. Si bulk"
-
-    # # Use raw pseudos for this example:
-    # raw_pseudos = [("Si.psf", 'Si')]
-
-    # calc.use_structure(s)
-    # calc.use_code(code)
-    # calc.use_parameters(parameters)
-    # calc.use_basis(basis)
-    # calc.use_kpoints(kpoints)
-
-    # Pseudo business
-    #
-    # TODO: understand how to work with pseudo families
-    # calc.use_pseudos_from_family(pseudo_family)
-    #
-    # for fname, kind in raw_pseudos:
-    #     absname = op.realpath(op.join(op.dirname(__file__), "..", "pseudos", fname))
-    #     pseudo, created = PsfData.get_or_create(absname, use_first=True)
-
-        # if created:
-        #     print("\nCreated the pseudo for {}".format(kind))
-        # else:
-        #     print("\nUsing the pseudo for {} from DB: {}".format(
-        #         kind, pseudo.pk))
-
-    # Attach pseudo node to the calculation
-    # calc.use_pseudo(pseudo, kind=kind)
-
-    # calc.store_all()
-    # print("created calculation with PK={}".format(calc.pk))
-    # subfolder, script_filename = calc.submit_test()
-    # print("Test_submit for calculation (uuid='{}')".format(calc.uuid))
-
-    # assert op.isfile(op.join(op.relpath(subfolder.abspath), script_filename))
-    # print("Submit file in {}".format(
-    #     op.join(op.relpath(subfolder.abspath), script_filename)))
-
-    # assert op.isfile(op.join(op.relpath(subfolder.abspath), 'aiida.fdf'))
-    # assert op.isfile(op.join(op.relpath(subfolder.abspath), 'Si.psf'))
