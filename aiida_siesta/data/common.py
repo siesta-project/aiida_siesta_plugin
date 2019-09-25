d4b5198018b1a871de93fe5d3e9f6aad61d55209
Alberto Garcia <albertog@icmab.es>
2019-09-25 09:27:27 +0200

    Cosmetic change in data/common.py

--- aiida_siesta/data/common.py
+++ aiida_siesta/data/common.py
@@ -5,14 +5,14 @@ from aiida_siesta.data.psml import PsmlData
 
 
 def get_pseudos_from_structure(structure, family_name):
-    """
-    Given a family name (a Siesta pseudo group in the DB, possibly
-    with mixed psf and psml pseudopotentials) and a AiiDA structure,
-    return a dictionary associating each kind name with its object
-    (PsfData or PsmlData).
+    """Given a family name (a Siesta pseudo group in the DB, possibly with
+    mixed psf and psml pseudopotentials) and an AiiDA structure
+    object, return a dictionary associating each 'kind' name in the
+    structure with its object (PsfData or PsmlData).
 
-    :raise MultipleObjectsError: if more than one pseudeo for the same element is
-       found in the group.
+    :raise MultipleObjectsError: if more than one pseudo for the same
+       element is found in the group.  
+
     :raise NotExistent: if no pseudo for an element in the group is
        found in the group.
 
