7b3f9aa4dc8fb98113ed82cecb0b3218d6176378
Alberto Garcia <albertog@icmab.es>
2019-10-02 14:34:49 +0200

    Migrate test_siesta_scf_fail example

--- aiida_siesta/examples/plugins/siesta/test_siesta_scf_fail.py
+++ aiida_siesta/examples/plugins/siesta/test_siesta_scf_fail.py
@@ -3,35 +3,26 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
-__copyright__ = u"Copyright (c), 2015, ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE (Theory and Simulation of Materials (THEOS) and National Centre for Computational Design and Discovery of Novel Materials (NCCR MARVEL)), Switzerland and ROBERT BOSCH LLC, USA. All rights reserved."
-__license__ = "MIT license, see LICENSE.txt file"
-__version__ = "0.7.0"
-__contributors__ = "Andrea Cepellotti, Victor Garcia-Suarez, Alberto Garcia, Emanuele Bosoni"
 
 #
-# This is an example of a calculation that will end in a FAILED state due
-# to lack of scf convergence in the allotted number of iterations.
-# To restart, either in a verdi shell or via a script, do:
-#
-# c = load_node(PK)
-# c2 = c.create_restart(force_restart=True)
-# c2.store_all()
-# c2.submit()
-#
-# (You can use the 'test_siesta_restart.py' script)
+# This is an example of a calculation that will end in a FINISHED
+# state but with a non-zero exit code, due to lack of scf convergence
+# in the allotted number of iterations.
 
 import sys
 import os
 
-from aiida.common.example_helpers import test_and_get_code
-from aiida.common.exceptions import NotExistent
+from aiida.engine import submit
+from aiida.orm import load_code
+from aiida_siesta.calculations.siesta import SiestaCalculation
+from aiida.plugins import DataFactory
 
 ################################################################
 
 PsfData = DataFactory('siesta.psf')
-ParameterData = DataFactory('parameter')
 KpointsData = DataFactory('array.kpoints')
 StructureData = DataFactory('structure')
+Dict = DataFactory('dict')
 
 try:
     dontsend = sys.argv[1]
@@ -43,162 +34,145 @@ try:
         raise IndexError
 except IndexError:
     print(("The first parameter can only be either "
-                          "--send or --dont-send"), file=sys.stderr)
+           "--send or --dont-send"),
+          file=sys.stderr)
     sys.exit(1)
 
 try:
     codename = sys.argv[2]
 except IndexError:
-    codename = 'siesta4.0.1@parsons'
+    codename = 'siesta4.0.1@kelvin'
 
-# If True, load the pseudos from the family specified below
-# Otherwise, use static files provided
-auto_pseudos = False
-
-queue = None
-settings = None
-
-code = test_and_get_code(codename, expected_code_type='siesta.siesta')
-
-alat = 5.430 # angstrom
-cell = [[0.5*alat, 0.5*alat, 0.,],
-        [0., 0.5*alat, 0.5*alat,],
-        [0.5*alat, 0., 0.5*alat,],
-       ]
-
-# Si
-# This was originally given in the "ScaledCartesian" format
 #
+#------------------Code
+#
+code = load_code(codename)
+
+#--------------- Structure
+#
+#    Bulk Silicon
+#
+alat = 5.430  # angstrom
+cell = [
+    [
+        0.5 * alat,
+        0.5 * alat,
+        0.,
+    ],
+    [
+        0.,
+        0.5 * alat,
+        0.5 * alat,
+    ],
+    [
+        0.5 * alat,
+        0.,
+        0.5 * alat,
+    ],
+]
+
 s = StructureData(cell=cell)
-s.append_atom(position=(0.000*alat,0.000*alat,0.000*alat),symbols=['Si'])
-s.append_atom(position=(0.250*alat,0.250*alat,0.250*alat),symbols=['Si'])
+s.append_atom(position=(0.000 * alat, 0.000 * alat, 0.000 * alat),
+              symbols=['Si'])
+s.append_atom(position=(0.250 * alat, 0.250 * alat, 0.250 * alat),
+              symbols=['Si'])
 
+#----------------------------------------------
 
-elements = list(s.get_symbols_set())
+options = {
+    "queue_name": "debug",
+    "max_wallclock_seconds": 1700,
+    "resources": {
+        "num_machines": 1,
+        "num_mpiprocs_per_machine": 1,
+    }
+}
 
-if auto_pseudos:
-    valid_pseudo_groups = PsfData.get_psf_groups(filter_elements=elements)
+#-------------------------- Settings ---------------------------------
+#
+settings_dict = {'additional_retrieve_list': ['aiida.BONDS', 'aiida.EIG']}
+settings = Dict(dict=settings_dict)
 
-    try:
-        #pseudo_family = sys.argv[3]
-        pseudo_family = 'lda-ag'
-    except IndexError:
-        print("Error, auto_pseudos set to True. You therefore need to pass as second parameter", file=sys.stderr)
-        print("the pseudo family name.", file=sys.stderr)
-        print("Valid PSF families are:", file=sys.stderr)
-        print("\n".join("* {}".format(i.name) for i in valid_pseudo_groups), file=sys.stderr)
-        sys.exit(1)
+#---------------------------------------------
+# Code-specific operational parameters
+#
+params_dict = {
+    'xc-functional': 'LDA',
+    'xc-authors': 'CA',
+    'spin-polarized': True,
+    'meshcutoff': '40.000 Ry',
+    'dm-numberpulay': 4,
+    'dm-mixingweight': 0.3,
+    'dm-tolerance': 1.e-3,
+    'max-scfiterations': 3,
+    'scf-must-converge': True,
+    'md-typeofrun': 'cg',
+    'md-numcgsteps': 0
+}
 
-    try:
-        PsfData.get_psf_group(pseudo_family)
-    except NotExistent:
-        print("auto_pseudos is set to True and pseudo_family='{}',".format(pseudo_family), file=sys.stderr)
-        print("but no group with such a name found in the DB.", file=sys.stderr)
-        print("Valid PSF groups are:", file=sys.stderr)
-        print(",".join(i.name for i in valid_pseudo_groups), file=sys.stderr)
-        sys.exit(1)
+parameters = Dict(dict=params_dict)
 
-parameters = ParameterData(dict={
-                'xc:functional': 'LDA',
-                'xc:authors': 'CA',
-                'spinpolarized': True,
-                'meshcutoff': '40.000 Ry',
-                'dm:numberpulay': 4,
-                'dm:mixingweight': 0.3,
-                'dm:tolerance': 1.e-3,
-                'max-scfiterations': 3,
-                'scf-must-converge': True,
-                'Solution-method': 'diagon',
-                'electronic-temperature': '25 meV',
-                'md-typeofrun': 'cg',
-                'md-numcgsteps': 0,
-                'md-maxcgdispl': '0.1 Ang',
-                'md-maxforcetol': '0.04 eV/Ang',
-                'writeforces': True,
-                'writecoorstep': True
-                })
+# ---------------------Basis Set Info -----------------------------------
+# The basis dictionary follows the 'parameters' convention
+#
+basis_dict = {
+    'pao-energy-shift':
+    '300 meV',
+    '%block pao-basis-sizes':
+    """
+Si DZP                    
+%endblock pao-basis-sizes""",
+}
 
-basis = ParameterData(dict={
-'pao-energy-shift': '300 meV',
-'%block pao-basis-sizes': """
-Si DZP                    """,
-})
+basis = Dict(dict=basis_dict)
 
+#-------------------------------------------
 kpoints = KpointsData()
 
 # method mesh
 kpoints_mesh = 4
-kpoints.set_kpoints_mesh([kpoints_mesh,kpoints_mesh,kpoints_mesh])
+kpoints.set_kpoints_mesh([kpoints_mesh, kpoints_mesh, kpoints_mesh])
 
-
-# (the object settings is optional)
-settings_dict={'test_key': 'test_value'}
-settings = ParameterData(dict=settings_dict)
-
-calc = code.new_calc()
-calc.label = "Si_bulk"
-calc.description = "Siesta scf non-convergence test"
-calc.set_max_wallclock_seconds(100) 
-
-calc.set_resources({"num_machines": 1})
-#code_mpi_enabled =  False
-#try:
-#    code_mpi_enabled =  code.get_extra("mpi")
-#except AttributeError:
-#    pass
-#calc.set_withmpi(code_mpi_enabled)
-
-#calc.set_custom_scheduler_commands("#SBATCH --account=ch3")
-
-if queue is not None:
-    calc.set_queue_name(queue)
-
-calc.use_structure(s)
-calc.use_parameters(parameters)
-calc.use_basis(basis)
-
-if auto_pseudos:
-    try:
-        calc.use_pseudos_from_family(pseudo_family)
-        print("Pseudos successfully loaded from family {}".format(pseudo_family))
-    except NotExistent:
-        print ("Pseudo or pseudo family not found. You may want to load the "
-               "pseudo family, or set auto_pseudos to False.")
-        raise
-else:
-    raw_pseudos = [("Si.psf", 'Si')]
-
-    for fname, kinds, in raw_pseudos:
-      absname = os.path.realpath(os.path.join(os.path.dirname(__file__),
-                                            "data",fname))
-      pseudo, created = PsfData.get_or_create(absname,use_first=True)
-      if created:
+#------------------------------------------------
+pseudos_list = []
+raw_pseudos = [("Si.psf", 'Si')]
+for fname, kinds, in raw_pseudos:
+    absname = os.path.realpath(
+        os.path.join(os.path.dirname(__file__), "data", fname))
+    pseudo, created = PsfData.get_or_create(absname, use_first=True)
+    if created:
         print("Created the pseudo for {}".format(kinds))
-      else:
-        print("Using the pseudo for {} from DB: {}".format(kinds,pseudo.pk))
-        
-      # Attach pseudo node to the calculation
-      calc.use_pseudo(pseudo,kind=kinds)
+    else:
+        print("Using the pseudo for {} from DB: {}".format(kinds, pseudo.pk))
+    pseudos_list.append(pseudo)
 
-calc.use_kpoints(kpoints)
+#---------------------------------------------
 
-
-if settings is not None:
-    calc.use_settings(settings)
+inputs = {
+    'structure': s,
+    'parameters': parameters,
+    'code': code,
+    'basis': basis,
+    'kpoints': kpoints,
+    'pseudos': {
+        'Si': pseudos_list[0],
+    },
+    'metadata': {
+        'options': options,
+        'label': "Bulk Si short scf cycle"
+    }
+}
 
 if submit_test:
-    subfolder, script_filename = calc.submit_test()
-    print("Test_submit for calculation (uuid='{}')".format(
-        calc.uuid))
-    print("Submit file in {}".format(os.path.join(
-        os.path.relpath(subfolder.abspath),
-        script_filename
-        )))
-else:
-    calc.store_all()
-    print("created calculation; calc=Calculation(uuid='{}') # ID={}".format(
-        calc.uuid,calc.dbnode.pk))
-    calc.submit()
-    print("submitted calculation; calc=Calculation(uuid='{}') # ID={}".format(
-        calc.uuid,calc.dbnode.pk))
+    inputs["metadata"]["dry_run"] = True
+    inputs["metadata"]["store_provenance"] = False
+    process = submit(SiestaCalculation, **inputs)
+    print("Submited test for calculation (uuid='{}')".format(process.uuid))
+    print("Check the folder submit_test for the result of the test")
 
+else:
+    process = submit(SiestaCalculation, **inputs)
+    print("Submitted calculation; ID={}".format(process.pk))
+    print("For information about this calculation type: verdi process show {}".
+          format(process.pk))
+    print("For a list of running processes type: verdi process list")
