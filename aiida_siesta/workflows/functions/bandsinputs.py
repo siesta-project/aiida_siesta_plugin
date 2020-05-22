f141d9f45e08f6783e58c49fb9a34b484c6a1454
Emanuele Bosoni <bosonie@tcd.ie>
2020-05-22 17:30:59 +0200

    Update the files to meet pre-commit requirements.

--- aiida_siesta/workflows/functions/bandsinputs.py
+++ aiida_siesta/workflows/functions/bandsinputs.py
@@ -1,6 +1,5 @@
-from aiida.engine import calcfunction, workfunction
-from aiida.orm import Bool
 from aiida_siesta.workflows.functions.protocols import ProtocolRegistry
+
 
 class SiestaBandsInputsGenerator(ProtocolRegistry):
     """
@@ -8,20 +7,21 @@ class SiestaBandsInputsGenerator(ProtocolRegistry):
     as well a builder for SiestaBaseWorkChain, but with correct inputs to perform
     a band structure analysis on a structure (without previous relaxation!!)
     """
-    
+
     _calc_types = {
-            "bands": { 'code_plugin': 'siesta.siesta',
-                            'description': 'These are calculations used for' 
-                            'the main run of the code, running an scf and then the bands'
-                          }
-            }
+        "bands": {
+            'code_plugin': 'siesta.siesta',
+            'description': 'These are calculations used for'
+            'the main run of the code, running an scf and then the bands'
+        }
+    }
     _path_generators = {
-            "seekpath" :
-                "primitive cell always used. Always ok",
-             "legacy" :
-                "To be used only if it is important to keep unchanged the cell shape"
-                "of the input sturcture (no primitive cell). Not guaranteed to give correct paths",
-                }
+        "seekpath":
+        "primitive cell always used. Always ok",
+        "legacy":
+        "To be used only if it is important to keep unchanged the cell shape"
+        "of the input sturcture (no primitive cell). Not guaranteed to give correct paths",
+    }
 
     @classmethod
     def get_calc_types(cls):
@@ -29,9 +29,9 @@ class SiestaBandsInputsGenerator(ProtocolRegistry):
 
     @classmethod
     def get_calc_type_schema(cls, key):
-        if key in cls._calc_types:
+        try:
             return cls._calc_types[key]
-        else:
+        except KeyError:
             raise ValueError("Wrong calc_type: no calc_type {} implemented".format(key))
 
     @classmethod
@@ -42,39 +42,37 @@ class SiestaBandsInputsGenerator(ProtocolRegistry):
     def get_builder(cls, structure, calc_engines, protocol, path_generator):
 
         from aiida_siesta.workflows.base import SiestaBaseWorkChain
-        from aiida.orm import (Str, KpointsData, Dict, StructureData)
+        from aiida.orm import (Str, KpointsData, Dict)
         from aiida.orm import load_code
         from aiida.tools import get_explicit_kpoints_path
-        
+
         if protocol not in cls.get_protocol_names():
             import warnings
             warnings.warn("no protocol implemented with name {}, using default standard".format(protocol))
-            protocol=cls.get_default_protocol_name()
+            protocol = cls.get_default_protocol_name()
 
-        protocol_dict=cls.get_protocol(protocol)
+        protocol_dict = cls.get_protocol(protocol)
 
         #K points
         kpoints_mesh = KpointsData()
         kpoints_mesh.set_cell_from_structure(structure)
-        kp_dict=protocol_dict["kpoints"]
-        kpoints_mesh.set_kpoints_mesh_from_density(distance=kp_dict["distance"],offset=kp_dict["offset"])
+        kp_dict = protocol_dict["kpoints"]
+        kpoints_mesh.set_kpoints_mesh_from_density(distance=kp_dict["distance"], offset=kp_dict["offset"])
 
         #Parameters, including scf and relax options
         #scf
         parameters = protocol_dict["parameters"].copy()
         meshcutoff = 0
-        min_meshcutoff = parameters["min_meshcut"] # In Rydberg (!)
+        min_meshcutoff = parameters["min_meshcut"]  # In Rydberg (!)
         del parameters["min_meshcut"]
         atomic_heuristics = protocol_dict["atomic_heuristics"]
         for kind in structure.get_kind_names():
-            try:
+            if atomic_heuristics[kind]:
                 cutoff = atomic_heuristics[kind]['cutoff']
                 meshcutoff = max(meshcutoff, cutoff)
-            except:
-                pass  # No problem. No heuristics, no info
-        meshcutoff = max(min_meshcutoff,meshcutoff)  
-        parameters["meshcutoff"]=str(meshcutoff)+" Ry"
-        #Walltime equal to scheduler, prevents calc to be killed by scheduler (it is implemented 
+        meshcutoff = max(min_meshcutoff, meshcutoff)
+        parameters["meshcutoff"] = str(meshcutoff) + " Ry"
+        #Walltime equal to scheduler, prevents calc to be killed by scheduler (it is implemented
         #in WorkChain as well, but this generator is more general
         parameters['max-walltime'] = calc_engines["bands"]["options"]["max_wallclock_seconds"]
 
@@ -86,23 +84,15 @@ class SiestaBandsInputsGenerator(ProtocolRegistry):
         pseudo_fam = protocol_dict["pseudo_family"]
 
         #Bands
-        if path_generator=="legacy":
+        if path_generator == "legacy":
             legacy_kpath_parameters = {
                 'kpoint_distance': 0.05  # In units of b1, b2, b3 (Around 20 points per side...)
-                }
-            result = get_explicit_kpoints_path(
-                    structure,
-                    method='legacy',
-                    **legacy_kpath_parameters)
+            }
+            result = get_explicit_kpoints_path(structure, method='legacy', **legacy_kpath_parameters)
             ok_structure = structure
         else:
-            seekpath_kpath_parameters = {
-                'reference_distance': 0.02,
-                'symprec': 0.0001
-                }
-            result = get_explicit_kpoints_path(
-            structure,
-            **seekpath_kpath_parameters)
+            seekpath_kpath_parameters = {'reference_distance': 0.02, 'symprec': 0.0001}
+            result = get_explicit_kpoints_path(structure, **seekpath_kpath_parameters)
             ok_structure = result['primitive_structure']
         bandskpoints = KpointsData()
         bandskpoints = result['explicit_kpoints']
@@ -118,4 +108,3 @@ class SiestaBandsInputsGenerator(ProtocolRegistry):
         builder.bandskpoints = bandskpoints
 
         return builder
-
