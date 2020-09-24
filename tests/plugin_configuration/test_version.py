7ce52a1f1c1bd57860be81c7fb9719b403bee4c3
Emanuele <bosonie@tcd.ie>
2020-09-24 15:29:01 +0200

    Fix the version in the docs and test it.
    And the authors in the copyright section of the docs.

--- tests/plugin_configuration/test_version.py
+++ tests/plugin_configuration/test_version.py
@@ -13,3 +13,9 @@ def test_version():
     from aiida_siesta.parsers.stm import STMParser
     assert STMParser._version == aiida_siesta.__version__
 
+    with open("aiida_siesta/docs/conf.py") as fil:
+        for line in fil.readlines():
+            if "version =" in line:
+                s=line
+    assert str(s.split()[2][2:-1]) == str(aiida_siesta.__version__)
+
