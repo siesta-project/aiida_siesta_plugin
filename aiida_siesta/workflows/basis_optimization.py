a1b90d063ec3d2ad98baca02bbda8857a84db860
Emanuele Bosoni <bosonie@tcd.ie>
2022-02-14 18:56:56 +0100

    Restructured the optimizer.
    Now the user can select the schema for optimization playing with the variables
    "global_energy_shift", "global_split_norm" and "charge_confinement".
    Added the possibility to run a check with an extra orbital after the basis
    sizes checks. The orbital to add must be specified in the
    `add_orbital` input.
    Finally allow to define a siesta_base.basis input with info that will be carried
    on along the entire optimization. For instance pao-non-perturbative-polarization
    or reparametrize-pseudos. This input can not contain pao-basis-sizes or
    pao blocks. It allows energy shift and split norm, but these will be
    removed in the last optimization step.

--- aiida_siesta/workflows/basis_optimization.py
+++ aiida_siesta/workflows/basis_optimization.py
@@ -1,11 +1,48 @@
-from aiida.engine import calcfunction, WorkChain, ToContext
+from aiida.engine import WorkChain, ToContext, if_
 from aiida import orm
 from aiida_siesta.workflows.simplex_basis import SimplexBasisOptimization
+from aiida_siesta.workflows.base import SiestaBaseWorkChain
 from aiida_siesta.workflows.iterate import SiestaIterator
+from aiida_siesta.utils.tkdict import FDFDict
 
 
-@calcfunction
-def extract_pao_block(**ions):
+def set_variables_in_pao_mod(ion_name, variables, n, l, pao_mod, global_splitnorm, charge_conf):  # noqa
+    """
+    Changes the pao_mod dictionary to include variables for obtimization
+    """
+    ang_to_bohr = 1.8897161646321
+    save_z1 = pao_mod._gen_dict[n][l][1]
+    for z in pao_mod._gen_dict[n][l]:
+        string = ion_name + str(n) + str(l) + str(z)
+        if z == 1:
+            suggest_radius = round(pao_mod._gen_dict[n][l][z] * ang_to_bohr, 6)
+            if suggest_radius * 2 < 12.5:
+                variables[string] = [1.5, 12.5, suggest_radius]
+            else:
+                variables[string] = [1.5, suggest_radius * 2, suggest_radius]
+            pao_mod._gen_dict[n][l][z] = "$" + string
+            if charge_conf:
+                if pao_mod._gen_occu[n][l][z] == 0.0:
+                    string2 = ion_name + str(n) + str(l) + "Zconf"
+                    variables[string2] = [1, 6, 3]
+                    pao_mod._conf_dict = {"Q": {n: {l: ["$" + string2]}}}
+        else:
+            if global_splitnorm:
+                pao_mod._gen_dict[n][l][z] = 0.0
+            else:
+                init_fac = round(pao_mod._gen_dict[n][l][z] / save_z1, 6)
+                if init_fac >= 0.95:
+                    init_fac = 0.94
+                variables[string] = [0.15, 0.95, init_fac]
+                pao_mod._gen_dict[n][l][z] = "-$" + string
+
+    if global_splitnorm:
+        variables["splitnorm"] = [0.05, 0.65, 0.15]
+
+    return variables
+
+
+def extract_pao_block(global_splitnorm, global_enrgyshift, charge_conf, **ions):
     """
     From the outputs ions, create a basis block with the variables for the optimization.
     It also selects range of the variables and initial point. This choice is not so
@@ -19,45 +56,130 @@ def extract_pao_block(**ions):
     * second or more zetas: start is 1/z (so 0.5 for z=2, 0.333.. for z=3, ...), min is 0.15,
       max is 0.95
     """
-    ang_to_bohr = 1.8897161646321
     variables = {}
     card = "\n"
-    for ion_name, ion in ions.items():  #pylint: disable=too-many-nested-blocks
+    for ion_name, ion in ions.items():
         pao_mod = ion.get_pao_modifier()
         for n in pao_mod._gen_dict:  #pylint: disable=invalid-name
             for l in pao_mod._gen_dict[n]:  # noqa
-                #save_z1 = pao_mod._gen_dict[n][l][1]
-                for z in pao_mod._gen_dict[n][l]:
-                    string = ion_name + str(n) + str(l) + str(z)
-                    if z == 1:
-                        suggest_radius = round(pao_mod._gen_dict[n][l][z] * ang_to_bohr, 6)
-                        if suggest_radius * 2 < 12.5:
-                            variables[string] = [1.5, 12.5, suggest_radius]
-                        else:
-                            variables[string] = [1.5, suggest_radius * 2, suggest_radius]
-                        pao_mod._gen_dict[n][l][z] = "$" + string
-                    else:
-                        #init_fac = round(pao_mod._gen_dict[n][l][z] / save_z1, 6)
-                        #if init_fac >= 0.95:
-                        #    init_fac = 0.94
-                        value_start = round(float(1 / z), 2)
-                        variables[string] = [0.15, 0.95, value_start]
-                        pao_mod._gen_dict[n][l][z] = "-$" + string
+                if global_enrgyshift:
+                    for z in pao_mod._gen_dict[n][l]:
+                        pao_mod._gen_dict[n][l][z] = 0.0
+                else:
+                    variables = set_variables_in_pao_mod(
+                        ion_name, variables, n, l, pao_mod, global_splitnorm, charge_conf
+                    )
         card += pao_mod.get_pao_block() + "\n"
 
     card += "%endblock pao-basis"
-    basis = orm.Dict(
-        dict={
-            "%block pao-basis": card,
-            "reparametrize-pseudos": True,
-            "restricted-radial-grid": False,
-            "pao-split-tail-norm": True
-        }
-    )
 
-    simpl = orm.Dict(dict=variables)
+    basis_dict = {"%block pao-basis": card}
+    if global_enrgyshift:
+        basis_dict.update({"pao-split-norm": "$splitnorm", "pao-energy-shift": "$energyshift meV"})
+        variables["splitnorm"] = [0.05, 0.65, 0.15]
+        variables["energyshift"] = [0.5, 50.5, 5]
+    if global_splitnorm:
+        basis_dict.update({"pao-split-norm": "$splitnorm"})
 
-    return {"basis": basis, "simplex": simpl}
+    #simpl = orm.Dict(dict=variables)
+    return basis_dict, variables
+    #return {"basis": basis, "simplex": simpl}
+
+
+def add_orbitals(orb_dict, **ions):
+    """
+    From the outputs ions, create a basis block with the variables for the optimization.
+    It also selects range of the variables and initial point.
+    First zeta radia are variables and the split-norm value. Also a charge confinament is added
+    for empty orbitals. The charge value is a variable to optimize.
+    """
+    l_dict = {"s": 0, "p": 1, "d": 2, "f": 3}
+    card = "\n"
+    one_changed = False
+    for ion_name, ion in ions.items():
+        to_change = orb_dict.get(ion_name, None)
+        n = int(to_change[0])  #pylint: disable=invalid-name
+        l = to_change[1]  # noqa
+        z = int(to_change[2])
+        pao_mod = ion.get_pao_modifier()
+        try:
+            pao_mod.add_orbital("Ang", 0.0, n, l_dict[l])
+            one_changed = True
+        except ValueError:
+            continue
+        if z == 2:
+            pao_mod.add_polarization(n, l_dict[l])
+
+        card += pao_mod.get_pao_block() + "\n"
+
+    card += "%endblock pao-basis"
+
+    if not one_changed:
+        return None
+
+    return card
+
+
+def validate_basis_opt(value, _):
+    """
+    Integrate the `basis` validator of the SiestaBaseWorkChain with a check
+    on the presence of "%block pao-basis" and 'pao-basis-size'. They are not allowed because
+    they are set automatically by the workflow.
+    Also remove the check on the presence of a '$' character, that was implemented
+    in the ForBasisOptWorkChain class.
+    """
+    if value:
+        orig_out = SiestaBaseWorkChain.spec().inputs["basis"].validator(value, _)
+        if orig_out is not None:
+            return orig_out
+        for key in value.get_dict():
+            if FDFDict.translate_key(key) == "%block paobasis":
+                return "the `basis` dictionary can not contain the key '%block pao-basis', is set by the algorithm"
+            if FDFDict.translate_key(key) == "paobasissize":
+                return "the `basis` dictionary can not contain the key 'pao-basis-size', is set by the algorithm"
+
+
+def validate_opt_schema(value, _):
+    """
+    Validate the `optimization_schema` port
+    """
+    if value:
+        if value["global_energy_shift"].value and not value["global_split_norm"].value:
+            return "Not supported global_energy_shift=True AND global_split_norm=False. Change `optimization_schema`."
+        if value["global_energy_shift"].value and value["charge_confinement"].value:
+            return "charge_confinement incompatible with global_energy_shift=True. Change `optimization_schema`."
+
+
+def validate_add_orbital(value, _):
+    """
+    Validate the `add_orbital` input port.
+    Imposes a signature of the dict of this kind:
+        {"Ca": "3d1", "Sr": "4d1"}
+    Meaning the key is an element and value is the orbital to add
+    At the moment we have suppot only for elemental symbols? Should we extend to
+    kind names?
+    """
+    from aiida.common.constants import elements
+
+    if value:
+        symbols = [v['symbol'] for v in elements.values()]
+        for key, val in value.get_dict().items():
+            if key not in symbols:
+                return "Every key of `add_orbital` dictionary must be a symbol of the periodic table"
+            if not isinstance(val, str) or not len(val) == 3:
+                return "The value of each item in the `add_orbital` dict must be a string 'nlz', e.g. '3d1'."
+            try:
+                int(val[0])
+            except ValueError:
+                return "The value of each item in the `add_orbital` dict must be a string 'nlz', e.g. '3d1'."
+            try:
+                int(val[2])
+            except ValueError:
+                return "The value of each item in the `add_orbital` dict must be a string 'nlz', e.g. '3d1'."
+            if val[1] not in ["s", "p", "d", "f"]:
+                return "The 'l' in the string 'nlz' in `add_orbital` dict has s, p, d and f as allowed values."
+            if int(val[2]) not in [1, 2]:
+                return "The 'z' in the string 'nlz' in `add_orbital` dict  has 1 and 2 as allowed values."
 
 
 class BasisOptimizationWorkChain(WorkChain):
@@ -68,28 +190,36 @@ class BasisOptimizationWorkChain(WorkChain):
     @classmethod
     def define(cls, spec):
         super().define(spec)
-        spec.expose_inputs(
-            SimplexBasisOptimization, exclude=('metadata', 'simplex.variables_dict', 'siesta_base.basis')
-        )
+        spec.expose_inputs(SimplexBasisOptimization, exclude=('metadata', 'simplex.variables_dict'))
         spec.input('basis_sizes', valid_type=orm.List, default=lambda: orm.List(list=["DZ", "DZP", "TZ"]))
         spec.input('sizes_monitored_quantity', valid_type=orm.Str, default=lambda: orm.Str("as_simplex"))
-        spec.input('non_perturbative_pol', valid_type=orm.Bool, default=lambda: orm.Bool(False))
+        spec.input('add_orbital', valid_type=orm.Dict, required=False, validator=validate_add_orbital)
+        spec.input_namespace('optimization_schema', validator=validate_opt_schema)
+        spec.input('optimization_schema.global_energy_shift', valid_type=orm.Bool, default=lambda: orm.Bool(False))
+        spec.input('optimization_schema.global_split_norm', valid_type=orm.Bool, default=lambda: orm.Bool(False))
+        spec.input('optimization_schema.charge_confinement', valid_type=orm.Bool, default=lambda: orm.Bool(False))
+
+        spec.inputs["siesta_base"]["basis"].validator = validate_basis_opt
+
         spec.output('optimal_basis_block', valid_type=orm.Dict)
+
         spec.outline(
             cls.run_sizes,
+            cls.check_sizes,
+            if_(cls.should_add_orbitals)(cls.run_extra, cls.check_extra),
             cls.run_optimizer,
             cls.run_results,
         )
+
         spec.exit_code(200, 'ERROR_SIZES_RUN', message='The SiestaIterator running pao sizes failed')
         spec.exit_code(201, 'ERROR_OPT', message='The basis optimization failed, probably not sufficient steps')
+        spec.exit_code(202, 'ERROR_EXTRA_RUN', message='The basis optimization failed, probably not sufficient steps')
 
     def run_sizes(self):
         """
         Run the iterator trying different basis sizes
         """
         siesta_inputs = self.exposed_inputs(SimplexBasisOptimization).siesta_base
-        if self.inputs.non_perturbative_pol.value:
-            siesta_inputs["basis"] = orm.Dict(dict={"pao-non-perturbative-polarization-orbitals": True})
         sizes_list = self.inputs.basis_sizes.get_list()
         run_sizes = self.submit(
             SiestaIterator,
@@ -100,17 +230,15 @@ class BasisOptimizationWorkChain(WorkChain):
         self.report(f'Launched SiestaIterator<{run_sizes.pk}> to test basis sizes.')
         return ToContext(sizes_run=run_sizes)
 
-    def run_optimizer(self):
+    def check_sizes(self):
         """
-        Extract basis size with lowest "sizes_monitored_quantity", create a starting pao block and
-        run the simplex optimizer varying all the cutoff radius.
-        The "sizes_monitored_quantity" is the quantity used to discriminate which is the best
-        basis size. A check is than on what basis size gives the smallest "sizes_monitored_quantity".
-        This quantity can be defined in input. By default it is the one used for the simplex and
-        specified in the input "simplex.output_name".
+        Check what is the size with minimum of `sizes_monitored_quantity`.
         """
         if not self.ctx.sizes_run.is_finished_ok:
             return self.exit_codes.ERROR_SIZES_RUN
+        for calc in self.ctx.sizes_run.called:
+            if not calc.is_finished_ok:
+                return self.exit_codes.ERROR_SIZES_RUN
 
         self.report('Concluded calculations to test basis sizes. Analyzing results.')
 
@@ -127,15 +255,116 @@ class BasisOptimizationWorkChain(WorkChain):
 
         self.report(f'Looking at {monitoring_quantity}, {good_calc.inputs.basis["paobasissize"]} is favourable')
 
-        if "ion_files" in good_calc.get_outgoing().nested():
-            ions = good_calc.get_outgoing().nested()["ion_files"]
+        self.ctx.monitoring_quantity = monitoring_quantity
+        return ToContext(good_sizes_calc=good_calc)
 
-        pao_opt_all = extract_pao_block(**ions)
+    def should_add_orbitals(self):
+        """
+        Check on the need of an extra step to check extra orbitals
+        """
+        if 'add_orbital' not in self.inputs:
+            return False
+
+        struct = self.exposed_inputs(SimplexBasisOptimization).siesta_base.structure
+        present_kinds = [kind.symbol for kind in struct.kinds]
+
+        for key in self.inputs['add_orbital'].get_dict():
+            if key in present_kinds:
+                return True
+
+        message = (
+            'Port `add_orbital` is set in input, but the keys of its dictionary does ' +
+            'not correnspond to any kind in input structure. Skip check on additional orbitals.'
+        )
+
+        self.report(message)
+
+        return False
+
+    def run_extra(self):
+        """
+        Run an extra calculation addind d orbitals
+        """
+        if "ion_files" in self.ctx.good_sizes_calc.get_outgoing().nested():
+            ions = self.ctx.good_sizes_calc.get_outgoing().nested()["ion_files"]
+
+        pao_block = add_orbitals(self.inputs.add_orbital.get_dict(), **ions)
+
+        if pao_block is None:
+            self.report("Input `add_orbital` included, but no orbital could be succesfully added. Skip")
+            return None
 
         siesta_inputs = self.exposed_inputs(SimplexBasisOptimization).siesta_base
-        siesta_inputs["basis"] = pao_opt_all["basis"]
+
+        if "basis" in siesta_inputs:
+            new_basis = siesta_inputs["basis"].clone()
+        else:
+            new_basis = orm.Dict(dict={})
+
+        new_basis["%block pao-basis"] = pao_block
+
+        siesta_inputs["basis"] = new_basis
+
+        run_extra = self.submit(SiestaBaseWorkChain, **siesta_inputs)
+
+        self.report(
+            f'Launched SiestaBaseWorkChain<{run_extra.pk}> with additional orbitals specified in `add_orbital`.'
+        )
+
+        return ToContext(extra_run=run_extra)
+
+    def check_extra(self):
+        """
+        Check the outputs of the extra run with additional orbitals
+        """
+        extra_run = self.ctx.extra_run
+        monitoring_quantity = self.ctx.monitoring_quantity
+
+        if not extra_run.is_finished_ok:
+            return self.exit_codes.ERROR_EXTRA_RUN
+
+        self.report('Concluded calculations to test additional_orbitals. Analyzing results.')
+
+        monitoring_quantity_value = extra_run.outputs.output_parameters[monitoring_quantity]
+
+        if monitoring_quantity_value <= self.ctx.good_sizes_calc.outputs.output_parameters[monitoring_quantity]:
+            self.ctx.good_sizes_calc = extra_run
+            self.report(f'Additional orbitals led to a lower {monitoring_quantity}')
+        else:
+            self.report(f'Additional orbitals did not lead to a lower {monitoring_quantity}')
+
+    def run_optimizer(self):
+        """
+        basis size. A check is than on what basis size gives the smallest "sizes_monitored_quantity".
+        This quantity can be defined in input. By default it is the one used for the simplex and
+        specified in the input "simplex.output_name".
+        """
+
+        if "ion_files" in self.ctx.good_sizes_calc.get_outgoing().nested():
+            ions = self.ctx.good_sizes_calc.get_outgoing().nested()["ion_files"]
+
+        global_enrgyshift = self.inputs.optimization_schema.global_energy_shift.value
+        global_splitnorm = self.inputs.optimization_schema.global_split_norm.value
+        charge_conf = self.inputs.optimization_schema.charge_confinement.value
+
+        basis_dict, variables = extract_pao_block(global_splitnorm, global_enrgyshift, charge_conf, **ions)
+
+        siesta_inputs = self.exposed_inputs(SimplexBasisOptimization).siesta_base
+
+        if "basis" in siesta_inputs:
+            new_basis = siesta_inputs["basis"].clone()
+            translated_dict = FDFDict(new_basis.get_dict())
+            for key in translated_dict:
+                if key in ("paosplitnorm", "paoenergyshift"):
+                    new_basis.attributes.pop(translated_dict.get_last_untranslated_key(key), None)
+            new_basis.update_dict(basis_dict)
+        else:
+            new_basis = orm.Dict(dict=basis_dict)
+
+        siesta_inputs["basis"] = new_basis
+
         simplex_inputs = self.exposed_inputs(SimplexBasisOptimization).simplex
-        simplex_inputs["variables_dict"] = pao_opt_all["simplex"]
+        simplex_inputs["variables_dict"] = orm.Dict(dict=variables)
         simp_wc_inputs = {
             'siesta_base': siesta_inputs,
             'simplex': simplex_inputs,
